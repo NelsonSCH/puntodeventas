1c4cfb4b2aa73a294e6c95fffb80b94f6347b64c
Nelson Medina <nelsonschaab@gmail.com>
2022-05-06 00:47:16 -0300

    Proyecto creado y configuración inicial.

---
+++ src/main/java/com/analistas/ventas/web/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.analistas.ventas.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = { "/", "/home" })
+    public String verHome(Model model) {
+        model.addAttribute("titulo", "Mi Aplicacion Spring Boot");
+
+        return "home";
+    }
+}
